a7319a3c7a352fb15ca9187e440ebdd1ceb9b6fc
Zhandos <zhandos38@gmail.com>
2021-05-24 20:45:33 +0600

    fixes

--- frontend/views/static/people.php
+++ frontend/views/static/people.php
@@ -6,6 +6,7 @@ $this->params['breadcrumbs'][] = ['label' => Yii::t('site', 'О центре'), 
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <h2><?= Yii::t('site', 'Населению') ?></h2>
+<?php if (Yii::$app->language === 'ru'): ?>
 <div class="row">
     <div class="col-md-12">
         <iframe src="/files/Положение об окозании платных услуг.PDF" frameborder="0" width="100%" height="420px"></iframe>
@@ -14,6 +15,13 @@ $this->params['breadcrumbs'][] = $this->title;
         <iframe src="/files/Прейскурант 04-21.PDF" frameborder="0" width="100%" height="420px"></iframe>
     </div>
 </div>
+<?php else: ?>
+    <div class="row">
+        <div class="col-md-12">
+            <iframe src="/files/Бағалардың тізімі.PDF" frameborder="0" width="100%" height="420px"></iframe>
+        </div>
+    </div>
+<?php endif; ?>
 <p>
     Договор по предоставлению медицинской помощи в рамках гарантированного объема бесплатной медицинской помощи, заключаемого между пациентом и медицинской организацией
     Обязательное социальное медицинское страхование
