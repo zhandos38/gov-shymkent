ae1076e5069af69006b8302d64c770a3123d14b4
Zhandos <zhandos38@gmail.com>
2021-06-14 23:50:19 +0600

    fixes

--- frontend/views/site/zakup.php
+++ frontend/views/site/zakup.php
@@ -7,7 +7,8 @@ use yii\helpers\Url; ?>
 <div class="container mt-80">
     <div class="row">
         <div class="col-lg-12 p-40">
-            <iframe src="/files/zakup.PDF" height="800" frameborder="0" style="border:0; width: 100%" allowfullscreen=""></iframe>
+            <a href="/files/zakup.PDF">Скачать государственные закупки</a> <br>
+            <a href="/files/File0001.PDF">Скачать протокол об утверждении итогов по закупкам</a> <br>
         </div>
     </div>
 </div>
